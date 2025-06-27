d36ca48ec1580a7be3c7ec0451dba9920c43fc74
KHALIDIIN12 <khalidinho53@gmail.com>
2025-06-27 14:20:43 +0300

    Complete Objects Lab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Write your solution in this file!
+
+const employee = {
+  name: "Ada Lovelace",
+  streetAddress: "1 Infinite Loop"
+};
+
+
+function updateEmployeeWithKeyAndValue(emp, key, value) {
+  return { ...emp, [key]: value };
+}
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(emp, key, value) {
+  emp[key] = value;
+  return emp;
+}
+
+function deleteFromEmployeeByKey(emp, key) {
+  const { [key]: _removed, ...newObj } = emp;
+  return newObj;
+}
+
+
+function destructivelyDeleteFromEmployeeByKey(emp, key) {
+  delete emp[key];
+  return emp;
+}
+
+
+module.exports = {
+  employee,
+  updateEmployeeWithKeyAndValue,
+  destructivelyUpdateEmployeeWithKeyAndValue,
+  deleteFromEmployeeByKey,
+  destructivelyDeleteFromEmployeeByKey
+};
